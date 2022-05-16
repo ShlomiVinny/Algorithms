77d5ffa5f6def32de05322cc71fd4ffb83834f9f
Shlomi Vinny <44550994+ShlomiVinny@users.noreply.github.com>
2022-05-16 17:31:46 +0300

    Update PlusOne.js

--- PlusOne.js
+++ PlusOne.js
@@ -5,8 +5,11 @@
  */
 var plusOne = function(digits) {
     // get the last digit in the array
-    let i = -1; // index of -1 is equivalent to digits[digits.length - 1]
+    let i = -1; 
     let lastDigit = digits.at(i);
+    // index of -1 is equivalent to digits[digits.length - 1]
+    // because im going to iterate over the array, member by member starting from the right, i is preferably negative as the indexes were using
+    
     while (true) {
         if (lastDigit === 9) { // if the last digit is 9, we need to zero it out and add one to the next digit
             digits[digits.length + i] = 0;
@@ -14,10 +17,13 @@ var plusOne = function(digits) {
             i--; // reduce the index to get the next digit
             lastDigit = digits.at(i); // set the next digit as last
             continue; // rerun loop 
-        } else if (lastDigit === undefined) { // if the digit does not exist we get an undef, therefore we must expand the array and add another member at the start (index = 0)
-            digits.unshift(0); // unshift a 0 which will be turned to a 1 in the next line
         }
-        //else if lastDig is not a 9 and is not undef, just add a 1
+        else if (lastDigit === undefined) { 
+            // if the digit does not exist we get an undef, therefore we must expand the array and add another member at the start (index = 0)
+            digits.unshift(0);
+            // unshift a 0 which will be turned to a 1 in the next line
+        }
+        //else if lastDigit is not a 9 and is not undef, just add a 1
         digits[digits.length + i] = digits[digits.length + i] + 1;
         break;
     }
